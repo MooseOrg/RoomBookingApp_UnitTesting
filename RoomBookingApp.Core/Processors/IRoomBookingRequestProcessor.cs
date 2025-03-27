7ee31215189782555c544e8386a5fee944145c43
Kim Reneil Paule <kim.paule@mvp.net.ph>
2025-03-27 15:17:09 +0800

    Implement unit test on API layer

---
+++ RoomBookingApp.Core/Processors/IRoomBookingRequestProcessor.cs
@@ -0,0 +1,7 @@
+﻿using RoomBookingApp.Core.Models;
+
+namespace RoomBookingApp.Core.Processors;
+public interface IRoomBookingRequestProcessor
+{
+    RoomBookingResult BookRoom(RoomBookingRequest bookingRequest);
+}
